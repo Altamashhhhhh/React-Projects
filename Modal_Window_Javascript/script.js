0a0aedd922dc3f5efd07e1ff0ec6c5342cc4361f
Shaikh Altamash <skaltamash10.99@gmail.com>
2024-11-14 23:15:23 +0530

    created base files

--- Modal_Window_Javascript/script.js
+++ Modal_Window_Javascript/script.js
@@ -17,6 +17,25 @@ for(let i=0; i<showModelBtns.length ; i++){
     showModelBtns[i].addEventListener("click" ,openModal )
 }
 
+document.addEventListener("keydown" , function (e){
+    if(e.key === "Escape" && !overlay.classList.contains("hidden")){
+        closeModal()
+    }
+    if(e.key === "Enter"  ){
+        openModal()
+    }
+    if(e.key === "Shift"){
+        document.querySelector("body").style.backgroundColor = "red"
+    }
+    if(e.key === "g"){
+        document.querySelector("body").style.backgroundColor = "green"
+    }
+    if(e.key === "b"){
+        document.querySelector("body").style.backgroundColor = "blue"
+    }
+    console.log(e.key)
+})
+
 
 closeModalBtn.addEventListener("click" , closeModal)
 overlay.addEventListener("click" , closeModal)
